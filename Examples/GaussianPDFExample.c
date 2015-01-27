f02e2aaafca391f7914d34f4b20dae0dbf89491a
Justin <JMarple@umass.edu>
2015-01-27 17:15:09 -0500

    Added licenses to several example programs

--- Examples/GaussianPDFExample.c
+++ Examples/GaussianPDFExample.c
@@ -10,6 +10,24 @@
 //
 // Dependencies:
 //   BNSLib.h in previous directory
+//
+// ------------------------------------------------------------------------
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// The author can be contacted by email at jmarple@umass.edu
+//
+// ------------------------------------------------------------------------
 #include "..\BNSLib.h"
 
 task main()
